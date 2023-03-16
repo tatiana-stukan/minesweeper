68633524a2ea0c5941d38207cf072976d09b8017
Your Name <you@example.com>
2023-03-16 12:13:11 +0100

    Corrected the WINSIZE and width/height in BoardRenderer

--- game.py
+++ game.py
@@ -17,16 +17,16 @@ default_size = 10
 def main():
     clock = Clock()
     pygame.init()
-    height = 24
-    width = 24
-    WINSIZE = (15 * width, 24+(15 * height))
+    width = 4
+    height = 5
+    WINSIZE = (15*width, 24+(15*height))
     print(WINSIZE)
     screen: pygame.Surface = pygame.display.set_mode(WINSIZE)
     init_main_tile()
     sys.setrecursionlimit(2500)
     difficulty = 1
 
-    board = BoardRenderer(WINSIZE[0], WINSIZE[1], height, width, difficulty=difficulty)
+    board = BoardRenderer(WINSIZE[0], WINSIZE[1], width, height, difficulty=difficulty)
     while not board.is_finished:
         board.render(screen)
         pygame.display.update()
@@ -34,10 +34,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                return
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-                return True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+                if event.key == pygame.K_r:
+                    return True
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     board.handle_click(event.pos, True)
